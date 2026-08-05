950d3694de297330809ab5af5d2a5d2041f61469
Lucas <sivlucas011@gmail.com>
2026-08-05 01:14:21 -0300

    implement contact management functions and main menu

--- agenda.py
+++ agenda.py
@@ -1,4 +1,36 @@
-print("1. Adicionar nome do contato: ")
-print("2. Adicionar número do contato: ")
-print("3. Adicionar email do contato: ")
-print("4. Adicionar contato aos favoritos: ")
+contatos = []
+
+def adicionar_contato():
+  nome = input("Digite o nome: ")
+  numero = input("Digite o número: ")
+  email = input("Digite o email: ")
+  contato = {"Nome":nome, "Número":numero, "Email":email}
+  contatos.append(contato) # append para adicionar "contato" na lista "contatos"
+  
+def ver_contatos():
+  if not contatos:
+    print("\nNão tem contatos!\n")
+  else:
+    for contato in contatos:  
+      print()
+      print("Nome:", contato["Nome"], "| Número:", contato["Número"], "| Email:", contato["Email"])
+    
+
+
+
+while True: # Faz rodar infinitamente até ser fechado
+  print("\nAgenda de Contatos")
+  print("\n1. Adicionar contato")
+  print("2. Adicionar aos favoritos")
+  print("3. Ver contatos")
+  print("4. Editar contatos")
+  print("5. Sair\n")
+
+  escolha = input("Digite sua escolha de 1 a 5: ")
+
+  if escolha == "1":
+    adicionar_contato()
+  elif escolha == "3":
+    ver_contatos()
+  elif escolha == "5":
+    break # Pra fechar o programa no terminal
